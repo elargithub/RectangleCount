99c66d2c397aff9bc64b0b104dde154ab8920475
elargithub <elargithub@citromail.hu>
2019-12-23 03:02:29 +0100

    Still without perpendicularity check

--- Program.cs
+++ Program.cs
@@ -16,23 +16,29 @@ namespace RectangleCount
         static List<List<StringBuilder>> ListOfSbLists = new List<List<StringBuilder>>();
         static void Main(string[] args)
         {
-            int pointsCount = 4 + 25;//rand.Next(25 - 4);
+            int pointsCount = 4 + 6;//rand.Next(25 - 4);
             int rectsCount = 0;
 
             InitDistinctDescendingPoints(pointsCount);
 
-            points.Clear();
 
-            points.Add(new Point(3, 5));
-            points.Add(new Point(2, 4));
-            points.Add(new Point(4, 4));
-            points.Add(new Point(3, 3));
-            points = points.OrderByDescending(p => p.Y).ThenByDescending(p => p.X).ToList();
-            pointsCount = points.Count();
+            // points.Clear();
+
+            // points.Add(new Point(3, 5));
+
+            // points.Add(new Point(2, 5));
+            // points.Add(new Point(4, 5));
+
+            // points.Add(new Point(2, 4));
+            // points.Add(new Point(4, 4));
+            // points.Add(new Point(3, 3));
+
+            // points.Add(new Point(5, 3));
+            // points.Add(new Point(4, 2));
+            // points = points.OrderByDescending(p => p.Y).ThenByDescending(p => p.X).ToList();
+            // pointsCount = points.Count();
 
             PlotToConsole();
-
-            InitNextListStringBuilder();
 
             for (int i = 0; i < pointsCount - 3; i++)
             {
@@ -53,10 +59,14 @@ namespace RectangleCount
                                 if (bl.Y == br.Y && bl.X == tl.X)
                                 {
                                     rectsCount++;
-                                    
+
+                                    if (rectsCount == 1)
+                                    {
+                                        InitNextListStringBuilder();
+                                    }
                                     if (rectsCount < 18)
                                     {
-                                        ListOfSbLists[0]  = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, ListOfSbLists[0]);
+                                        ListOfSbLists[0] = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, ListOfSbLists[0], false);
                                     }
                                     if (rectsCount == 18)
                                     {
@@ -64,7 +74,7 @@ namespace RectangleCount
                                     }
                                     if (rectsCount >= 18 && rectsCount < 36)
                                     {
-                                        ListOfSbLists[1]  = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, ListOfSbLists[1]);
+                                        ListOfSbLists[1] = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, ListOfSbLists[1], false);
                                     }
                                     break;
                                 }
@@ -74,34 +84,13 @@ namespace RectangleCount
                 }
             }
 
-            if (ListOfSbLists[0][0].Length > 1)
-                foreach (var SBList in ListOfSbLists)
-                {
-                    foreach (var sb in SBList)
-                        Console.WriteLine(sb);
+            PlotRes(rectsCount, "");
 
-                    Console.WriteLine();
-                }
-
-
-            Console.WriteLine("no of rects: " + rectsCount);
-
-
-            List<StringBuilder> sbList = new List<StringBuilder>();
-            for (int i = 0; i < 5 + maxYCoord; i++)
-            {
-                sbList.Add(new StringBuilder(" "));
-            }
 
             Console.WriteLine();
             Console.WriteLine("Oblique rectc ");
+            ListOfSbLists.Clear();
             rectsCount = 0;
-            sbList.Clear();
-
-            for (int i = 0; i < 5 + maxYCoord; i++)
-            {
-                sbList.Add(new StringBuilder(" "));
-            }
 
             for (int i = 0; i < pointsCount - 3; i++)
             {
@@ -126,10 +115,10 @@ namespace RectangleCount
 
                                 // check for perpendicularity top and left side
 
-                                for (int l = k + 1; l < pointsCount; l++)
+                                for (int l = j + 1; l < pointsCount; l++)
                                 {
                                     Point br = points[l];// bottom-right
-                                    if (br.X < tr.X)
+                                    // if (br.X < tr.X)
                                     {
                                         // def bottom side coord diff  
                                         int DxB = bl.X - br.X;
@@ -143,10 +132,24 @@ namespace RectangleCount
                                         if (DxB == DxT && DyB == DyT && DxR == DxL && DyR == DyL)
                                         {
                                             rectsCount++;
-                                            if (rectsCount < 12)
+
+                                            if (rectsCount == 1)
                                             {
-                                                sbList = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, sbList);
+                                                InitNextListStringBuilder();
                                             }
+                                            if (rectsCount < 18)
+                                            {
+                                                ListOfSbLists[0] = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, ListOfSbLists[0], true);
+                                            }
+                                            if (rectsCount == 18)
+                                            {
+                                                InitNextListStringBuilder();
+                                            }
+                                            if (rectsCount >= 18 && rectsCount < 36)
+                                            {
+                                                ListOfSbLists[1] = PlotRectsToSbList(new List<Point> { tl, tr, bl, br }, ListOfSbLists[1], true);
+                                            }
+
                                             break;
                                         }
                                     }
@@ -157,13 +160,7 @@ namespace RectangleCount
                 }
             }
 
-            if (sbList[0].Length > 1)
-                foreach (var sb in sbList)
-                    Console.WriteLine(sb);
-
-
-            Console.WriteLine("No. of oblique rects: " + rectsCount);
-
+            PlotRes(rectsCount);
         }
 
         private static void InitNextListStringBuilder()
@@ -254,7 +251,7 @@ namespace RectangleCount
             Console.WriteLine(borderStr);
             Console.WriteLine();
         }
-        private static List<StringBuilder> PlotRectsToSbList(List<Point> ps, List<StringBuilder> sbList)
+        private static List<StringBuilder> PlotRectsToSbList(List<Point> ps, List<StringBuilder> sbList, bool IsRotd)
         {
             Point tl = ps[0];// top-left
             Point tr = ps[1];// top-right
@@ -268,21 +265,55 @@ namespace RectangleCount
             sbList[2].Append(borderStr);
 
             int j = 0;
-            for (int i = maxYCoord; i >= 0; i--)
+            for (int y = maxYCoord; y >= 0; y--)
             {
                 StringBuilder rowStrB = new StringBuilder("|" + new String(' ', maxXCoord + 1) + "|", maxXCoord + 3);
 
-                while (j < ps.Count && ps[j].Y == i)
-                {
-                    rowStrB.Remove(ps[j].X + 1, 1);
-                    rowStrB.Insert(ps[j].X + 1, "+");
-                    j++;
-                }
-                sbList[3 + maxYCoord - i].Append(rowStrB);
+                sbList[3 + maxYCoord - y].Append(IsRotd ? GetRowStrB(y, ps, rowStrB) : GetRowStrB(ref j, y, ps, rowStrB));
             }
             sbList[4 + maxYCoord].Append(borderStr);
 
             return sbList;
+        }
+        private static StringBuilder GetRowStrB(ref int j, int y, List<Point> ps, StringBuilder rowStrB)
+        {
+            while (j < ps.Count && ps[j].Y == y)
+            {
+                SetStringBuilder(ps[j], rowStrB);
+                j++;
+            }
+            return rowStrB;
+        }
+        private static StringBuilder GetRowStrB(int y, List<Point> ps, StringBuilder rowStrB)
+        {
+            foreach (var p in ps)
+            {
+                if (p.Y == y)
+                {
+                    SetStringBuilder(p, rowStrB);
+                }
+            }
+            return rowStrB;
+        }
+        private static StringBuilder SetStringBuilder(Point p, StringBuilder sb)
+        {
+            sb.Remove(p.X + 1, 1);
+            sb.Insert(p.X + 1, "+");
+            return sb;
+        }
+
+        private static void PlotRes(int rectsCount, string kind = "oblique")
+        {
+            if (ListOfSbLists.Count > 0 && ListOfSbLists[0][0].Length > 1)
+                foreach (var SBList in ListOfSbLists)
+                {
+                    foreach (var sb in SBList)
+                        Console.WriteLine(sb);
+
+                    Console.WriteLine();
+                }
+
+            Console.WriteLine($"No. of {kind} rects: " + rectsCount);
         }
     }
 }
